839f4c180c9a511ed776add9e62345ab8805bb6f
llisnikowski <l.lisnikowski@gmail.com>
2023-10-21 18:25:33 +0200

    Changed template args names

--- include/llgl/Matrix.hpp
+++ include/llgl/Matrix.hpp
@@ -6,12 +6,12 @@
 namespace llgl 
 {
 
-template <size_t W, size_t H>
+template <size_t ROW, size_t COL>
 class Matrix
 {
 public:
-    using Row = std::array<float, H>;
-    using Array = std::array<Row, W>;
+    using Row = std::array<float, COL>;
+    using Array = std::array<Row, ROW>;
     
     constexpr Matrix();
     constexpr Matrix(Array array);
@@ -38,83 +38,83 @@ private:
 
 
 
-template <size_t W, size_t H>
-constexpr Matrix<W, H>::Matrix()
+template <size_t ROW, size_t COL>
+constexpr Matrix<ROW, COL>::Matrix()
 :array{}
 {
-    if constexpr(W == H){
-        for(int i = 0; i < W; i++){
+    if constexpr(ROW == COL){
+        for(int i = 0; i < ROW; i++){
             array[i][i] = 1.0f;
         }
     }
 }
 
-template <size_t W, size_t H>
-constexpr Matrix<W, H>::Matrix(Array array)
+template <size_t ROW, size_t COL>
+constexpr Matrix<ROW, COL>::Matrix(Array array)
 :array{array}
 {}
 
-template <size_t W, size_t H>
+template <size_t ROW, size_t COL>
 template <typename ...Ts>
-constexpr Matrix<W, H>::Matrix(Ts ...ts)
+constexpr Matrix<ROW, COL>::Matrix(Ts ...ts)
 :array{}
 {
-    static_assert(sizeof...(ts) == W * H);
+    static_assert(sizeof...(ts) == ROW * COL);
     this->fill<0>(ts...);
 }
 
-template <size_t W, size_t H>
-constexpr decltype(auto) Matrix<W, H>::operator [](std::size_t pos)
+template <size_t ROW, size_t COL>
+constexpr decltype(auto) Matrix<ROW, COL>::operator [](std::size_t pos)
 {
     return array[pos];
 }
 
-template <size_t W, size_t H>
-constexpr decltype(auto) Matrix<W, H>::operator [](std::size_t pos) const
+template <size_t ROW, size_t COL>
+constexpr decltype(auto) Matrix<ROW, COL>::operator [](std::size_t pos) const
 {
     return array[pos];
 }
 
-template <size_t W, size_t H>
-float *Matrix<W, H>::begin()
+template <size_t ROW, size_t COL>
+float *Matrix<ROW, COL>::begin()
 {
     return &array.front().front();
 }
 
-template <size_t W, size_t H>
-float *Matrix<W, H>::end()
+template <size_t ROW, size_t COL>
+float *Matrix<ROW, COL>::end()
 {
     return &array.back().back()+1;
 }
 
-template <size_t W, size_t H>
-const float *Matrix<W, H>::cbegin() const
+template <size_t ROW, size_t COL>
+const float *Matrix<ROW, COL>::cbegin() const
 {
     return &array.front().front();
 }
 
-template <size_t W, size_t H>
-const float *Matrix<W, H>::cend() const
+template <size_t ROW, size_t COL>
+const float *Matrix<ROW, COL>::cend() const
 {
     return &array.back().back()+1;
 }
 
-template <size_t W, size_t H>
+template <size_t ROW, size_t COL>
 template <std::size_t pos, typename ...Ts>
-constexpr void Matrix<W, H>::fill(float t, Ts... ts)
+constexpr void Matrix<ROW, COL>::fill(float t, Ts... ts)
 {
-    static_assert(pos < W * H);
-    array[pos/H][pos%H] = t;
+    static_assert(pos < ROW * COL);
+    array[pos/COL][pos%COL] = t;
     if constexpr (sizeof...(ts)){
         this->fill<pos+1>(ts...);
     } 
 }
 
-template <size_t W, size_t H>
-constexpr void Matrix<W, H>::foreach(IteratorFunc func)
+template <size_t ROW, size_t COL>
+constexpr void Matrix<ROW, COL>::foreach(IteratorFunc func)
 {
-    for(int row = 0; row < W; row++){
-        for(int col = 0; col < H; col++){
+    for(int row = 0; row < ROW; row++){
+        for(int col = 0; col < COL; col++){
             func(array[row][col], row, col);
         }
     }
@@ -123,19 +123,19 @@ constexpr void Matrix<W, H>::foreach(IteratorFunc func)
 } // namespace llgl
 
 
-template <size_t W1, size_t H1, size_t W2, size_t H2>
-auto operator *(llgl::Matrix<W1, H1> lhs, llgl::Matrix<W2, H2> rhs);
+template <size_t ROW1, size_t COL1, size_t ROW2, size_t COL2>
+auto operator *(llgl::Matrix<ROW1, COL1> lhs, llgl::Matrix<ROW2, COL2> rhs);
 
 
-template <size_t W1, size_t H1, size_t W2, size_t H2>
-auto operator *(llgl::Matrix<W1, H1> lhs, llgl::Matrix<W2, H2> rhs)
+template <size_t ROW1, size_t COL1, size_t ROW2, size_t COL2>
+auto operator *(llgl::Matrix<ROW1, COL1> lhs, llgl::Matrix<ROW2, COL2> rhs)
 {
-    static_assert(H1 == W2);
-    llgl::Matrix<W1, H2> mat;
+    static_assert(COL1 == ROW2);
+    llgl::Matrix<ROW1, COL2> mat;
     mat.foreach([&lhs, &rhs](float &value, size_t row, size_t col)
     {
         float sum = 0;
-        for(int i = 0; i < H1; i++){
+        for(int i = 0; i < COL1; i++){
             sum += lhs[row][i] * rhs[i][col];
         }
         value = sum;
