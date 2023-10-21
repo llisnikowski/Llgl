8b627063e464a1a901114cd106b3e129241bc506
llisnikowski <l.lisnikowski@gmail.com>
2023-10-21 11:15:27 +0200

    Basic functions

--- include/llgl/Matrix.hpp
+++ include/llgl/Matrix.hpp
@@ -1,27 +1,104 @@
 #pragma once
 #include <array>
+#include <type_traits>
 
 namespace llgl 
 {
 
 template <size_t W, size_t H>
-class Matrix {
+class Matrix
+{
 public:
-    using Row = std::array<float, W>;
-    using Array = std::array<Row, H>;
+    using Row = std::array<float, H>;
+    using Array = std::array<Row, W>;
+    
+    constexpr Matrix();
+    constexpr Matrix(Array array);
+    template <typename ...Ts>
+    constexpr Matrix(Ts ...ts);
 
-    constexpr decltype(auto) operator[] (std::size_t pos);
+    constexpr decltype(auto) operator [](std::size_t pos);
+    constexpr decltype(auto) operator [](std::size_t pos) const;
+
+    float *begin();
+    float *end();
+    const float *cbegin() const;
+    const float *cend() const;
 
 private:
-    Array array = {};
+    template <std::size_t pos, typename ...Ts>
+    constexpr void fill(float t, Ts... ts);
+
+    Array array;
 };
 
 
+template <size_t W, size_t H>
+constexpr Matrix<W, H>::Matrix()
+:array{}
+{}
 
 template <size_t W, size_t H>
-constexpr decltype(auto) Matrix<W, H>::operator[] (std::size_t pos)
+constexpr Matrix<W, H>::Matrix(Array array)
+:array{array}
+{}
+
+template <size_t W, size_t H>
+template <typename ...Ts>
+constexpr Matrix<W, H>::Matrix(Ts ...ts)
+:array{}
+{
+    static_assert(sizeof...(ts) == W * H);
+    this->fill<0>(ts...);
+}
+
+template <size_t W, size_t H>
+constexpr decltype(auto) Matrix<W, H>::operator [](std::size_t pos)
 {
     return array[pos];
 }
+
+template <size_t W, size_t H>
+constexpr decltype(auto) Matrix<W, H>::operator [](std::size_t pos) const
+{
+    return array[pos];
+}
+
+template <size_t W, size_t H>
+float *Matrix<W, H>::begin()
+{
+    return &array.front().front();
+}
+
+template <size_t W, size_t H>
+float *Matrix<W, H>::end()
+{
+    return &array.back().back()+1;
+}
+
+template <size_t W, size_t H>
+const float *Matrix<W, H>::cbegin() const
+{
+    return &array.front().front();
+}
+
+template <size_t W, size_t H>
+const float *Matrix<W, H>::cend() const
+{
+    return &array.back().back()+1;
+}
+
+template <size_t W, size_t H>
+template <std::size_t pos, typename ...Ts>
+constexpr void Matrix<W, H>::fill(float t, Ts... ts)
+{
+    static_assert(pos < W * H);
+    array[pos/H][pos%H] = t;
+    if constexpr (sizeof...(ts)){
+        this->fill<pos+1>(ts...);
+    } 
+}
+
+
 
 } // namespace llgl
--- test/Matrix_test.cpp
+++ test/Matrix_test.cpp
@@ -13,4 +13,51 @@ TEST(Matrix_test, operatonSquare)
     EXPECT_FLOAT_EQ(m[0][2], 0);
     EXPECT_FLOAT_EQ(m[1][2], 3.1);
     EXPECT_FLOAT_EQ(m[2][1], 0);
+    
+    const auto &mconst = m;
+    EXPECT_FLOAT_EQ(mconst[1][2], 3.1);
+
+}
+
+TEST(Matrix_test, beginAndEnd)
+{
+    llgl::Matrix<4, 3> m;
+    const llgl::Matrix<4, 3> mconst;
+    EXPECT_EQ(static_cast<void*>(&m), m.begin());
+    EXPECT_EQ(m.begin()+4*3, m.end());
+    EXPECT_EQ(static_cast<const void*>(&mconst), mconst.cbegin());
+    EXPECT_EQ(mconst.cbegin()+4*3, mconst.cend());
+}
+
+TEST(Matrix_test, emptyConstructor)
+{
+    llgl::Matrix<4, 3> m;
+
+    for(float value : m){
+        EXPECT_FLOAT_EQ(value, 0);
+    }
+}
+
+TEST(Matrix_test, constructorArray)
+{
+    constexpr llgl::Matrix<3, 2> m{
+        {{{1, 2}, {3, 4}, {5, 6}}}
+    };
+
+    for(int row = 0; row < 3; row++){
+        for(int col = 0; col < 2; col++){
+            EXPECT_FLOAT_EQ(m[row][col], row*2+col+1);
+        }
+    }
+}
+
+TEST(Matrix_test, constructorValues)
+{
+    constexpr llgl::Matrix<3, 2> m{1, 2, 3, 4, 5, 6};
+
+    for(int row = 0; row < 3; row++){
+        for(int col = 0; col < 2; col++){
+            EXPECT_FLOAT_EQ(m[row][col], row*2+col+1);
+        }
+    }
 }
